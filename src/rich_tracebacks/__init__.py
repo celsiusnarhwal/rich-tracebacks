cc2b78d66819b3edcd8e262d68c63d85b4a844f1
celsius narhwal <hello@celsiusnarhwal.dev>
2023-01-03 15:37:10 -0500

    Update __init__.py

--- src/rich_tracebacks/__init__.py
+++ src/rich_tracebacks/__init__.py
@@ -28,7 +28,7 @@ if os.getenv("RICH_TRACEBACKS"):
         config = {}
 
     config["suppress"] = [
-        _resolve_module(module) for module in config.get("suppress", ())
+        _resolve_module(module) for module in config.get("suppress", [])
     ]
 
     install(**config)
